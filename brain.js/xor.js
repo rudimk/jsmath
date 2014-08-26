22b6abd0a796636eabe9fa7dc7772dbb0fa33079
Rudi MK <rudi@mathharbor.com>
2014-08-26 09:54:38 +0000

    Added basic demos for neural networks using brain.js.

---
+++ brain.js/xor.js
@@ -0,0 +1,10 @@
+var brain = require('brain')
+var net = new brain.NeuralNetwork();
+
+net.train([{input: [0, 0], output: [0]},
+           {input: [0, 1], output: [1]},
+           {input: [1, 0], output: [1]},
+           {input: [1, 1], output: [0]}]);
+
+var output = net.run([1, 0]);
+console.log(output);
